647496f9d351cdefe908176faa5ba5571d625c5d
Zygimantas Kairys <zk@zkdeveloper.com>
2019-06-25 12:25:14 -0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Github Finder</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
